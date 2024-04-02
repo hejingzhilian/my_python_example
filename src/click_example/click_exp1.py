d27b5100c3e8ad9447383a87e51e6af6398bc796
hejingzhilian <1242173261@qq.com>
2024-04-02 12:58:19 +0000

    add client

--- src/click_example/click_exp1.py
+++ src/click_example/click_exp1.py
@@ -1,4 +1,31 @@
 import click
 
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def initdb():
+    click.echo('Initialized the database')
+
+@click.command()
+def dropdb():
+    click.echo('Dropped the database')
+
+cli.add_command(initdb)
+cli.add_command(dropdb)
+
+@click.group()
+def cli_sub():
+    pass
+
+@click.command()
+def sub_dropdb():
+    click.echo('sub_dropdb ')
+
+## add_command 即可以是command 也可以是 group
+cli_sub.add_command(sub_dropdb)
+
+cli.add_command(cli_sub)
 if __name__ == "__main__":
-    print("hello work")
+    cli()
